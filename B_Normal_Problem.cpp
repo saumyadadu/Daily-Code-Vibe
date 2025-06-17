b52b819078866d44f44d384583884cdff7c58a41
saumyadadu <164731018+saumyadadu@users.noreply.github.com>
2025-06-17 19:17:01 +0530

    Code Day 9

---
+++ B_Normal_Problem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='q'){
+                cout<<"p";
+            }
+            else if(s[i]=='p'){
+                cout<<"q";
+            }
+            else{
+                cout<<"w";
+            }
+        }
+        cout<<endl;
+    }
+}
